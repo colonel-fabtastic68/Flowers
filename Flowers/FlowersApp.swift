dfdbddf2fc80e57393aeaa77d7683e06ab27560f
bakercobb <bakerjcobb@gmail.com>
2025-12-28 00:39:11 -0500

    Initial Commit

---
+++ Flowers/FlowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowersApp.swift
+//  Flowers
+//
+//  Created by Baker Cobb on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FlowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlowersTests/FlowersTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlowersTests.swift
+//  FlowersTests
+//
+//  Created by Baker Cobb on 12/28/25.
+//
+
+import Testing
+@testable import Flowers
+
+struct FlowersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
